4ca649e0099800a9d6888a60daa572f65e4d8445
Armand Raynor <13355630+mvnd06@users.noreply.github.com>
2022-12-19 18:21:42 -0800

    Add create and delete functions to webhook manager

--- webhook-manager.js
+++ webhook-manager.js
@@ -1,25 +1,31 @@
+const { response } = require("express");
 const request = require("request-promise");
 require("dotenv").config();
 
 class WebhookManager {
-  constructor() {}
-
-  createWebhook() {
-    console.log("creating web hook");
-    const twitter_oauth = {
+  constructor() {
+    this.auth = {
       consumer_key: process.env.TWITTER_CONSUMER_KEY,
       consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
       token: process.env.TWITTER_ACCESS_TOKEN,
       token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
     };
+    this.env = process.env.TWITTER_WEBHOOK_ENV;
+  }
 
-    const authorization = this.generateAuthorizationString(process.env.TWITTER_CONSUMER_KEY, process.env.TWITTER_ACCESS_TOKEN);
+  createWebhook() {
+    this.deleteWebhookIfNeeded();
+    console.log("Creating web hook");
+
     // request options
     var request_options = {
-      url: 'https://api.twitter.com/1.1/account_activity/all/' + process.env.TWITTER_WEBHOOK_ENV + '/webhooks.json',
-      oauth: twitter_oauth,
+      url:
+        "https://api.twitter.com/1.1/account_activity/all/" +
+        this.env +
+        "/webhooks.json",
+      oauth: this.auth,
       headers: {
-        'Content-type': 'application/x-www-form-urlencoded'
+        "Content-type": "application/x-www-form-urlencoded",
       },
       json: true,
       form: {
@@ -31,7 +37,7 @@ class WebhookManager {
     request
       .post(request_options)
       .then(function (body) {
-        console.log("test");
+        console.log('Successfully created webhook!');
         console.log(body);
       })
       .catch(function (error) {
@@ -39,11 +45,50 @@ class WebhookManager {
       });
   }
 
-  generateAuthorizationString(consumerKey, accessToken) {
-    return `OAuth oauth_consumer_key="${consumerKey}", 
-    oauth_nonce="GENERATED", oauth_signature="GENERATED", 
-    oauth_signature_method="HMAC-SHA1", oauth_timestamp="GENERATED", 
-    oauth_token="${accessToken}", oauth_version="1.0"`;
+  getWebhook(callback) {
+    var request_options = {
+      url:
+        "https://api.twitter.com/1.1/account_activity/all/" +
+        this.env +
+        "/webhooks.json",
+      oauth: this.auth,
+    };
+
+    // GET request to retreive webhook config
+    request
+      .get(request_options)
+      .then(function (body) {
+        if (body == '[]') {
+          console.log("No exisiting webhooks found");
+          return;
+        }
+        console.log("Found Webhook: " + body);
+        if (callback) {
+          callback(body);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  deleteWebhookIfNeeded() {
+    this.getWebhook((response) => {
+      var webhook_id = JSON.parse(response)[0].id;
+      console.log("Deleting webhook config:", webhook_id);
+
+      var request_options = {
+        url:
+          "https://api.twitter.com/1.1/account_activity/all/" +
+          this.env +
+          "/webhooks/" +
+          webhook_id +
+          ".json",
+        oauth: this.auth,
+        resolveWithFullResponse: true,
+      };
+      return request.delete(request_options);
+    });
   }
 }
 
